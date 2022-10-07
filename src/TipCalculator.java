c4b73df86165644773e0dba68a61cd33d099bea5
unknown <BT_3W2_31@BKTECH.LOCAL>
2022-10-07 08:59:47 -0400

    Initial commit

---
+++ src/TipCalculator.java
@@ -0,0 +1,14 @@
+public class TipCalculator {
+    //instance variables
+    private int num;
+    private int tip;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, int tipPercentage) {
+        num = numPeople;
+        tip = tipPercentage;
+
+    }
+
+
+}
---
+++ src/TipCalculatorRunner.java
@@ -0,0 +1,6 @@
+public class TipCalculatorRunner {
+    public static void main(String[] args)
+    {
+
+    }
+}
